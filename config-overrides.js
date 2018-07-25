b2a1debc9dbc2f4e5ebdd0b79ace1c35ff636c40
Mariusz Krok <mariusz.krok@g.pl>
2018-07-25 19:45:23 +0200

    Move to scss loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,20 @@
 /* config-overrides.js */
+const { compose } = require('react-app-rewired');
+const rewireLodash = require('react-app-rewire-lodash');
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
+const rewireVendorSplitting = require("react-app-rewire-vendor-splitting");
 
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
-}
+module.exports = function(config, env){
+    const rewires = compose(
+        rewireLodash,
+        rewireVendorSplitting
+    );
+
+    if (env === 'production') {
+        config = rewireWebpackBundleAnalyzer(config, env, {
+            analyzerMode: 'static',
+            reportFilename: 'report.html'
+        })
+    }
+    return rewires(config, env);
+};
---
+++ src/vendors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    vendors: [
+        "react",
+        "react-dom",
+        "react-router-dom",
+        "react-router-redux",
+        "react-redux",
+        "redux"
+    ]
+};
